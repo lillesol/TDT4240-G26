d9d56b968b9288ada8d2844092e98ca5903993bb
unknown <olavsole@gmail.com>
2020-03-05 13:44:39 +0100

    init project

---
+++ app/src/main/java/com/TDT4240_G26/georush/MainActivity.java
@@ -0,0 +1,14 @@
+package com.TDT4240_G26.georush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
